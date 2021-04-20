63194e1c7fa7eee371519ffbdc7aefc1827da6ec
Abel Souza <abelsouzacosta@gmail.com>
2021-04-20 03:02:16 -0300

    chore: conectando ao banco de dados
    agora é possível se conectar com o banco de dados da aplicação
    através do método createConnection() dentro do arquivo /src/shared/typeorm/index.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,15 @@
-import app from './app';
+import 'reflect-metadata';
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import 'dotenv/config';
+import '@shared/typeorm';
+import router from './routes';
 
-const port = process.env.PORT;
+const app = express();
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(process.env.PORT);
